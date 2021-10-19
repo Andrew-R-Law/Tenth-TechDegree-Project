8585b60332a8817b1cea4728df7c2978d408bc02
Andrew-R-Law <andy.bob.law91@gmail.com>
2021-10-19 11:24:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# SQL-library-manager
+ Project 10 for the Web Dev. Tech Degree.
+
+Before starting the app, run 'npm install' in the terminal. This app uses "cookie-parser", "debug", "express", "http-errors", "morgan", "pug","sequelize", "sequelize-cli", and "sqlite3".
+
+Once all dependencies have been installed, run 'npm start' in the terminal.
+
+Then, navigate your browser to 'localhost:3000' to view the app.
+
+Currently, the app wipes the database each time it is started. To stop this from happening, delete or comment out the code in app.js on lines 20-26. 
+
+
---
+++ app.js
@@ -0,0 +1,38 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const { sequelize } = require('./models');
+var app = express();
+
+
+/*Tests connection and syncs database.*/
+
+(async () => {
+    try {
+        await sequelize.sync({alter: true});
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.log ('Unable to connect to the database:', error);
+    }
+});
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
